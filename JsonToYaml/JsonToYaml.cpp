9d9cc02d3fa2fe14a6740de92348da0fce5a6dd1
Elias Wennerlund <elias06wennerlund@gmail.com>
2024-05-16 14:12:16 +0200

    Added command line arguments

--- JsonToYaml/JsonToYaml.cpp
+++ JsonToYaml/JsonToYaml.cpp
@@ -4,18 +4,42 @@
 
 #include "Parser.h"
 
-int main() {
-	std::ifstream input("./array.json");
+int main(int argc, char* argv[]) {
+	if (argc == 1) {
+		std::cout <<
+			"JsonToYaml version 1.0.0\n"
+			"Use --help or -h to get full help\n";
+		return 0;
+	}
 
-	if (!input.is_open()) {
-		std::cout << "Unable to open file\n";
+	if (argv[1] == "--help" || argv[1] == "-h") {
+		std::cout <<
+			"JsonToYaml version 1.0.0\n"
+			"usage: jsontoyaml <input file> <output file>\n";
+		return 0;
+	}
+
+	if (argc != 3) {
+		std::cout <<
+			"Invalid usage\n"
+			"Use --help or -h to get full help\n";
 		return 1;
 	}
 
+	std::ifstream input(argv[1]);
+	if (!input.is_open()) {
+		std::cout << "Unable to open input file: " << argv[1];
+		return 1;
+	}
+
+	std::ofstream output(argv[2]);
+	if (!output.is_open()) {
+		std::cout << "Unable to open output file: " << argv[2];
+		return 1;
+	}
+	
 	Json json = ParseJson(input);
 	input.close();
-
-	std::ofstream output("./array.yaml");
 
 	YamlPrinter printer(json, output);
 	printer.Print();
